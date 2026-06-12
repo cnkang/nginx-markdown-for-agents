fcc335ff7b349899813d5c4f9656ee8b3196a17e
Kang <555836+cnkang@users.noreply.github.com>
2026-06-12 14:09:04 +0800

    fix(nginx-module): make local conf variable pointer-to-const in body_filter

--- components/nginx-module/src/ngx_http_markdown_streaming_impl.h
+++ components/nginx-module/src/ngx_http_markdown_streaming_impl.h
@@ -3466,7 +3466,7 @@ ngx_http_markdown_streaming_body_filter(
     ngx_http_request_t *r, ngx_chain_t *in)
 {
     ngx_http_markdown_ctx_t   *ctx;
-    ngx_http_markdown_conf_t  *conf;
+    const ngx_http_markdown_conf_t  *conf;
     ngx_chain_t               *fallback_cl;
     ngx_int_t                  rc;
     ngx_flag_t                 last_buf;
